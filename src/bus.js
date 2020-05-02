dfd6594b1596230be15590b0930e34fac5df189e
Ryan Chandler <ryan@chandler.codes>
2020-05-02 23:07:46 +0100

    review(todo comment link to polyfill)

--- src/bus.js
+++ src/bus.js
@@ -17,6 +17,7 @@ export default {
         }
 
         // TODO: deal with this for IE11 :(
+        // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Browser_compatibility#Polyfill
         window.dispatchEvent(new CustomEvent(`spruce:${name}`, {
             detail: data,
             bubbles: true

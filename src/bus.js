b0322cae4f5ea6cd095819a4a0d59fa6297ac308
Ryan Chandler <ryan@chandler.codes>
2020-08-27 11:12:33 +0100

    remove(ie11 todo)

--- src/bus.js
+++ src/bus.js
@@ -34,8 +34,6 @@ export default {
             })
         }
 
-        // TODO: deal with this for IE11 :(
-        // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Browser_compatibility#Polyfill
         window.dispatchEvent(new CustomEvent(`spruce:${name}`, {
             detail: data,
             bubbles: true

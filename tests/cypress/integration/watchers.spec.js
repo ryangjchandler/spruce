342423525c534346e6e7294b521e4a99d2f8ee6e
Ryan Chandler <ryangjchandler@gmail.com>
2020-12-17 15:49:44 +0000

    tests: array watchers

--- tests/cypress/integration/watchers.spec.js
+++ tests/cypress/integration/watchers.spec.js
@@ -5,11 +5,19 @@ describe('watchers', () => {
         cy.visit('/tests/cypress/fixtures/watchers/init.html')
 
         cy.get('p').should('have.text', 'bar')
-        cy.get('span').should('have.text', 'boo')
+        cy.get('[data-bar]').should('have.text', 'boo')
 
-        cy.get('button').click()
+        cy.get('[data-change-foo]').click()
 
         cy.get('p').should('have.text', 'car')
-        cy.get('span').should('have.text', 'bop')
+        cy.get('[data-bar]').should('have.text', 'bop')
+
+        cy.get('[data-todo-item]').should('have.length', 0)
+        cy.get('[data-todo-label]').should('have.text', 'Yes!')
+
+        cy.get('[data-add-todo]').click()
+
+        cy.get('[data-todo-item]').should('have.length', 1)
+        cy.get('[data-todo-label]').should('have.text', 'Yay!')
     })
 })

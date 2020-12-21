fa8e9f52e433151474165983314c79722ef781f7
Ryan Chandler <ryangjchandler@gmail.com>
2020-12-21 11:37:18 +0000

    feature: add support for watching objects

--- src/observable.js
+++ src/observable.js
@@ -2,22 +2,18 @@ import { isNullOrUndefined, isObject, isArray } from './utils'
 
 export const createObservable = (target, callbacks) => {
     Object.entries(target).forEach(([key, value]) => {
-        if (! isNullOrUndefined(value) && (isObject(value) || isArray(value))) {
-            if (isArray(value)) {
-                value['__key_name'] = key
-            }
-            
+        if (! isNullOrUndefined(value) && (isObject(value) || isArray(value))) {            
             target[key] = createObservable(value, callbacks)
         }
     })
 
     return new Proxy(target, {
-        set(target, key, value) {
+        set(target, key, value, receiver) {
             if (! isNullOrUndefined(value) && isObject(value)) {
                 value = createObservable(value, callbacks)
             }
 
-            callbacks.set(target, key, target[key] = value)
+            callbacks.set(target, key, target[key] = value, receiver)
 
             return true
         }

d707007c27942660a60a125a044fd28ba50de62a
Ryan Chandler <ryangjchandler@gmail.com>
2021-02-01 00:05:34 +0000

    fix(reset): allow resetting null stores

--- tests/cypress/integration/reset.spec.js
+++ tests/cypress/integration/reset.spec.js
@@ -4,10 +4,16 @@ describe('reset', () => {
     it('should re-render after state reset', () => {
         cy.visit('/tests/cypress/fixtures/reset')
 
-        cy.get('span').should('have.text', 'car')
+        cy.get('span[data-basic]').should('have.text', 'car')
 
-        cy.get('button').click()
+        cy.get('button[data-basic-update]').click()
 
-        cy.get('span').should('have.text', 'boo')
+        cy.get('span[data-basic]').should('have.text', 'boo')
+
+        cy.get('[data-null-store]').should('have.text', '')
+
+        cy.get('[data-null-store-update]').click()
+
+        cy.get('[data-null-store]').should('have.text', 'Ryan')
     })
 })
